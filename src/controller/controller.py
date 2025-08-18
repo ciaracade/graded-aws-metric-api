c3a79986dd677be94fa5a1978785abac0cba15c9
cadeci <ciaracade03@gmail.com>
2025-08-17 23:49:40 -0700

    wip: Impl controller

--- src/controller/controller.py
+++ src/controller/controller.py
@@ -3,8 +3,10 @@ Controller is the component that enables the interconnection between the views a
 the model so it acts as an intermediary
 """
 
-from model.model import modelConfig
+from model.model import modelConfig, VPC, Subnet
+import logging
 
+logger = logging.getLogger(__name__)
 
 class controllerConfig:
     def __init__(self, model: modelConfig):
